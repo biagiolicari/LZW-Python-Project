33b98af97c4b514b60153165b27a87274af3a6e4
Biagio Licari <biagiolicari7@gmail.com>
2019-02-16 13:04:24 +0100

    aggiunto controllo versione python

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+import sys
+
+if not sys.version_info.major >= 3 :
+    sys.exit("Sorry, only Python 3 is supported (yet)")
+    
 
 setup(name='Lempel-Ziv-Welch',
       version='1.0.0',
+      python_requires = '>= 3.5.2',
       install_requires=[
             'argparse',
             'setuptools',
